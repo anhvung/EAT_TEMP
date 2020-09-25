d0c691f99e99d3af65a26aeae015470e96f5e1e5
anhvung <59536014+anhvung@users.noreply.github.com>
2020-09-25 16:43:06 +0200

    first version beta

---
+++ assets/js/artistes.js
@@ -0,0 +1,40 @@
+
+var myvar = '<section id="wrapper">'+
+'						'+
+''+
+'						<!-- Content -->'+
+'							<div class="wrapper">'+
+'								<div class="inner">'+
+''+
+'									'+
+'									<h3 class="major">Nos artistes</h3>'+
+'									'+
+'									<section class="features">'+
+'										<article>'+
+'											<a href="anhvu.html" class="image imageartiste"><img src="images/moi.jpg" alt="" /></a>'+
+'											<h3 class="major">Anh-Vu NGUYEN</h3>'+
+'											'+
+'											<a href="anhvu.html" class="special">Explorer</a>'+
+'										</article>'+
+'										<article>'+
+'											<a href="44.html" class="image imageartiste"><img src="images/44.jpg" alt="" /></a>'+
+'											<h3 class="major">44</h3>'+
+'											'+
+'											<a href="44.html" class="special">Explorer</a>'+
+'										</article>'+
+'										<article>'+
+'											<a href="benj.html" class="image imageartiste"><img src="images/benj.jpg" alt="" /></a>'+
+'											<h3 class="major">Benj Tissot</h3>'+
+'											'+
+'											<a href="benj.html" class="special">Explorer</a>'+
+'										</article>'+
+'									</section>'+
+''+
+'								</div>'+
+'							</div>'+
+''+
+'					</section>';
+	
+
+
+document.write(myvar);
